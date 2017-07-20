42341143ccbfae4ffdcfb5bc7f929913c1011f34
Sujan <najussujan@gmail.com>
2017-07-19 22:53:20 -0400

    mongodb authentication success

---
+++ src/main/java/com/vote/talks/VoteMain.java
@@ -0,0 +1,49 @@
+package com.vote.talks;
+
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+import org.jongo.Jongo;
+import org.jongo.MongoCollection;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+public class VoteMain {
+
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+
+    @Value("${spring.data.mongodb.port}")
+    private int port;
+
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoteMain.class, args);
+    }
+
+    @Bean
+    public Jongo jongo() {
+        System.out.println(host+  port + database);
+        DB db;
+        try {
+            db = new MongoClient(host, port).getDB(database);
+        } catch (UnknownHostException e) {
+            throw new MongoException("Connection error : ", e);
+        }
+        return new Jongo(db);
+    }
+
+    @Bean
+    public MongoCollection users() {
+        return jongo().getCollection("users");
+    }
+
+    
+}
